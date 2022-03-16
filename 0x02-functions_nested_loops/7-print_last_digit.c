5f67f806756a827e57d8f08855ee073aa1555477
IamHalima <ozavizehalima@gmail.comgit config -- user.name IamHalima>
2022-03-16 23:04:39 +0000

    my file

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+
+#include "main.h"
+/**
+ * print_last_digit - A function that return the last digit of a number
+ * @n: number to check the last digit
+ * Return: the last digit of n
+ */
+
+int print_last_digit(int n)
+{
+	int lastNum;
+
+	lastNum = n % 10;
+	if (lastNum < 0)
+		lastNum = 0 - lastNum;
+	_putchar(lastNum + '0');
+	return (lastNum);
+}
