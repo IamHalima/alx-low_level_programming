ca23e9d779175b7c6ce21e9066c1af3d3fa9e3b3
IamHalima <ozavizehalima@gmail.comgit config -- user.name IamHalima>
2022-03-17 00:08:17 +0000

    my file

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,31 @@
+
+#include "main.h"
+/**
+ * jack_bauer - A function that prints every minutes of the day
+ */
+void jack_bauer(void)
+{
+	int i, j, k, l;
+	
+	for (i = '0'; i <= '2'; i++)
+	{
+		for (j = '0'; j <= '9'; j++)
+		{
+			if (i == '2' && j == '4')
+				                  break;
+			for (k = '0'; k <= '5'; k++)
+			{
+				for (1 = '0'; 1 <= '9'; 1++)
+				{                               _putchar(i);
+	      _putchar(j);
+	      _putchar(':');
+	      _putchar(k);
+	      _putchar(1);
+	      _putchar('\n');
+	                         }
+			}
+		}
+	}
+}
+
+							
