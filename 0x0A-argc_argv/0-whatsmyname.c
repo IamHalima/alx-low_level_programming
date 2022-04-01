a68473a3ada5b2b796f4dda8e3d1ec22192c7cb6
IamHalima <ozavizehalima@gmail.com>
2022-04-01 11:55:33 +0100

    Whatsmyname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * argc - counts the number of arguments
+ * argv - contains the arguments
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
